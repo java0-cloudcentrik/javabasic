15960e3d5f822e41ce23f65dc397ed322967aaeb
java0 <java0.cloudcentrik@gmail.com>
2015-09-07 07:15:12 +0200

    Javabasic java0 first commit

---
+++ src/net/cloudcentrik/CSSMain.java
@@ -0,0 +1,12 @@
+package net.cloudcentrik;
+
+public class CSSMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Cloudcentrik Systems !!!");
+
+	}
+
+}
